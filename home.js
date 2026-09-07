9970a69964ae5a0fc3de322588dd8534218f26fd
Aryan Rai <artanrai4444@gmail.com>
2026-09-07 20:51:28 +0530

    js file

---
+++ home.js
@@ -0,0 +1,5 @@
+let order_button = getElementById("order_btn");
+(onclick ="document.getElementById('menu').scrollIntoView({behavior:'smooth'})"); 
+document.querySelector("order_btn").addEventListener("click", function () {
+  window.location.href = "order.html";
+});
